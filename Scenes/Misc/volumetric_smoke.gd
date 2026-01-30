c344c8959805c297cbdc7d431d5d97731d3a8237
jakegit93 <jakejohnsonbusiness@gmail.com>
2026-01-31 01:11:08 +1000

    we can now draw a cube at the voxel that intersects with the origin of any object

--- Scenes/Misc/volumetric_smoke.gd
+++ Scenes/Misc/volumetric_smoke.gd
@@ -1,11 +1,20 @@
-#TODO
-#Functions are calling each other too much, we should provide the functions with the class agnostically 
-#Make vxl files their own resource type in a different script so we can pass them into Volume3D objects
-#Require a vxl resource per Volume3D object
-#3D noise textures (etc) should be resources of objects that interface with Volume3D
-#Extend per-voxel values for lighting system?
-#Add greedy mesher
-#Add noise3D support
+# TODO
+
+# Functions are calling each other too much, we should provide the functions with the class agnostically
+# and let the class instance do the logic.
+
+# Require a vxl resource per Volume3D object?
+
+# 3D noise textures (etc) should be resources of objects that interface with Volume3D
+
+# Extend per-voxel values for lighting system?
+
+# Add greedy mesher
+
+# Add noise3D support
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+@tool
 
 class_name Volume3D extends Node3D
 
@@ -18,15 +27,20 @@ class_name Volume3D extends Node3D
 
 @onready var grid_origin := self.global_position
 @onready var shared_mesh := BoxMesh.new()
+@onready var shared_mat := StandardMaterial3D.new()
 
 func _ready():
 	var grid := generate_grid()
 	write_grid_to_file(grid)
 
-	read_file("res://Assets/Volumes/TestVolume.vxl")
+	var voxel_voxelspace := worldspace_to_voxelspace(test_object.position)
+	var voxel_pos := get_voxel_position(voxel_voxelspace)
+	visualize_voxel(voxel_pos)
 
-func _physics_process(delta: float) -> void:
-	worldspace_to_voxelspace(test_object.position)
+	#read_file("res://Assets/Volumes/TestVolume.vxl")
+
+func _physics_process(_delta: float) -> void:
+	pass
 
 func generate_grid() -> PackedByteArray:
 	var grid_array := PackedByteArray()
@@ -52,7 +66,7 @@ func generate_grid() -> PackedByteArray:
 
 				#debug visuals
 				if occupied && debug == true:
-					visualizer(local_pos)
+					visualize_occupied(local_pos)
 
 	return grid_array
 
@@ -98,6 +112,7 @@ func write_header(file: FileAccess) -> void:
 	file.store_32(grid_height)
 	file.store_32(grid_depth)
 
+#gonna have to redo this potentially?
 func write_grid_to_file(grid: PackedByteArray) -> void:
 	var path := "res://Assets/Volumes/TestVolume.vxl"
 
@@ -106,6 +121,7 @@ func write_grid_to_file(grid: PackedByteArray) -> void:
 	file.store_buffer(grid)
 	file.close()
 
+#and this?
 func read_file(path: String):
 	var file := FileAccess.open(path, FileAccess.READ)
 	var file_type := file.get_pascal_string()
@@ -151,10 +167,21 @@ func read_file(path: String):
 func worldspace_to_voxelspace(pos: Vector3) -> Vector3i:
 	var origin := self.position 
 	var object_voxel_position : Vector3i = floor(pos - origin / voxel_size)
-	print(object_voxel_position)
 	return object_voxel_position
 
-func visualizer(pos: Vector3) -> void:
+func get_voxel_position(index: Vector3i) -> Vector3:
+	var current_pos := grid_origin
+	for z in index.z:
+		current_pos.z += voxel_size
+	for y in index.y:
+		current_pos.y += voxel_size
+	for x in index.x:
+		current_pos.x += voxel_size
+
+	print("voxel world position: ", current_pos)
+	return current_pos
+
+func visualize_occupied(pos: Vector3) -> void:
 	var shared_cube := MeshInstance3D.new()
 	shared_cube.mesh = shared_mesh
 	var mat := StandardMaterial3D.new()
@@ -169,4 +196,14 @@ func visualizer(pos: Vector3) -> void:
 	#if use_wireframe_debug:
 		#RenderingServer.set_debug_generate_wireframes(true)
 		#get_viewport().debug_draw = Viewport.DEBUG_DRAW_WIREFRAME
-	
+
+#we need to query the grid, and insert the mesh that aligns with the voxel position.	
+func visualize_voxel(pos: Vector3i) -> void:
+	var cube := MeshInstance3D.new()
+	cube.mesh = shared_mesh
+	cube.scale = Vector3.ONE * voxel_size * 0.99
+	shared_mat.albedo_color = Color(255,0,0)
+	cube.position = worldspace_to_voxelspace(pos)
+	print("voxel grid position: ", pos)
+	#cube.set_surface_override_material(0, shared_mat)
+	add_child(cube)
