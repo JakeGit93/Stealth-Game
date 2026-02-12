605e28549716f2b5168663bded5a58b6db696e32
jakegit93 <jakejohnsonbusiness@gmail.com>
2026-02-12 13:01:01 +1000

    changed the setup for expanding the smoke, not finished

--- Scenes/Misc/smokegrenade.gd
+++ Scenes/Misc/smokegrenade.gd
@@ -1,6 +1,15 @@
+#TODO:
+#add setters/getters for the radius property so we can access them with the tween to expand the smoke radius
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 class_name SmokeGrenade extends Node3D
 
 @export var grid : Volume3D
+@onready var radius : float:
+	set(value):
+		pass
+		#expand_smoke()
+
 
 
 func _ready() -> void:
@@ -9,31 +18,33 @@ func _ready() -> void:
 		set_process(false)
 		return
 
+	generate_nade(Vector3(5,0,5))
+	var smoke_tween = create_tween().tween_property(self, "radius", 5.0, 10.0)
+
 func _process(delta: float) -> void:
-	var tween = get_tree().create_tween()
-	#tween.tween_method(expand_nade.bind()) #finish this off!
+	pass
 
-func expand_nade(voxel_array : Array[Vector3i], radius : int, pos : Vector3) -> Array[Vector3i]:
-	var smoke_array : Array[Vector3i] = []
-	for i in range(voxel_array.size()):
-		var vox = grid.voxelspace_to_worldspace(voxel_array[i])
-		var dist = vox - pos
-		if dist.length_squared() <= radius * radius:
-			smoke_array.append(vox)
-			grid.visualize_voxel(vox)
+	
 
-	return smoke_array
-
-func generate_nade(pos: Vector3) -> Array[Vector3i]:
+func generate_nade(pos : Vector3) -> Array[Vector3i]:
 	var full_size = 100
 	var diameter = full_size * 0.1
-	var radius = floor(diameter * 0.5)
 	var center_voxel = grid.worldspace_to_voxelspace(pos)
-	print(to_global(pos))
+
 	var bounds_voxels: Array[Vector3i] = []
 	for x in range(-diameter, diameter):
 		for y in range(-diameter, diameter):
 			for z in range(-diameter, diameter):
 					bounds_voxels.append(center_voxel + Vector3i(x,y,z))
-	
+		
 	return bounds_voxels
+
+func expand_smoke(arr : Array[Vector3i], pos : Vector3) -> void:
+	var smoke_array: Array[Vector3] = []
+	for i in range(arr.size()):
+		var vox = grid.voxelspace_to_worldspace(arr[i])
+		var dist = vox - pos
+		if dist.length_squared() <= radius * radius:
+			smoke_array.append(vox)
+			grid.visualize_voxel(vox)
+	
