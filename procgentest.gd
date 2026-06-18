7803e0963f64ed43926b3f583020251a7a196526
jakegit93 <jakejohnsonbusiness@gmail.com>
2026-06-19 01:39:23 +1000

    testing volumetric smoke generation

--- procgentest.gd
+++ procgentest.gd
@@ -1,39 +1,28 @@
-extends Node3D
+extends MeshInstance3D
 
-@export var energy := 16
-@export var voxel_size := 1
-
+#need to implement everything and then merge with volumetric_smoke.gd once finished
 
 func _ready() -> void:
-	if energy >= 1:
-		pass
+	pass
 
+func _generate_shells() -> Dictionary:
+	var dict = []
+	return dict
 
-
-#we will need to check 6 directions for the origin voxel, storing the non-intersecting ones
-#in an array. Each voxel must reference it's neighbours.
-func _generate(position: Vector3, energy: int)-> void:
-	var check_left : bool
-	var check_right : bool
-	var check_forward : bool
-	var check_back : bool
-	var check_up : bool
-	var check_down : bool
-
+func intersects_geometry(pos: Vector3) -> bool:
 	var space_state := get_world_3d().direct_space_state
 	var shape := BoxShape3D.new()
 	shape.size = Vector3.ONE
 
 	var query := PhysicsShapeQueryParameters3D.new()
 	query.shape = shape
-	query.transform = Transform3D(Basis.IDENTITY, self.global_position)
+	query.transform = Transform3D(Basis.IDENTITY, pos)
 	query.collide_with_bodies = true
 	query.collide_with_areas = false
 
 	var result := space_state.intersect_shape(query, 1)
 
 	if result.size() != 0:
-		pass
+		return false
 	else:
-		energy -= 1
-		_generate()
+		return true
